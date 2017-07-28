b5ede69bcfacb19132c33d0bea26b5111debdbcc
Yoni Weill <jweill@google.com>
2017-07-28 23:20:16 +0300

    First commit.

---
+++ README.md
@@ -0,0 +1,18 @@
+# Enhanced keyboard navigation in Google Search
+
+This is a very simple Google Chrome extension that enhances the keyboard
+navigation in Google Search. Google provides an experimental keyboard navigation
+for search results, but it's often broken for me (and [not just for
+  me](https://goo.gl/1zMkYu)).
+
+## Keybindings
+
+* `↓`: Select next search result
+* `↑`: Select previous previous result
+* `/`: Focus on input search box
+* `Return`: Navigate to selected result
+* `Ctrl/⌘+Return`: Open selected result in new tab/window
+
+## Installation
+
+TODO
---
+++ google_keyboard_shortcuts.js
@@ -0,0 +1,57 @@
+const toggleResultHighlighting = (link) => {
+  link.classList.toggle('highlighted-search-result')
+};
+
+const addEventListeners = () => {
+  console.log('Starting addEventListeners');
+  let resultIndex = 0;
+  let results = document.querySelectorAll('h3.r a');
+  const updateHighlightedResult = (newResultIndex) => {
+    results[resultIndex].classList.remove('highlighted-search-result')
+    resultIndex = newResultIndex;
+    results[resultIndex].classList.add('highlighted-search-result')
+    results[resultIndex].focus();
+  };
+  // Highlight the first result when the page is loaded.
+  updateHighlightedResult(0);
+  key('down', (event) => {
+    let newIndex = resultIndex + 1;
+    if (newIndex == results.length) {
+      newIndex = 0;
+    }
+    updateHighlightedResult(newIndex);
+    event.stopPropagation();
+    event.preventDefault();
+  });
+  key('up', (event) => {
+    let newIndex = resultIndex - 1;
+    if (newIndex == -1) {
+      newIndex = results.length - 1;
+    }
+    updateHighlightedResult(newIndex);
+    event.stopPropagation();
+    event.preventDefault();
+  });
+  key('⌘+return, ctrl+return', (event) => {
+    let link = results[resultIndex];
+    window.open(link.attr('href'));
+    event.stopPropagation();
+    event.preventDefault();
+  });
+  key('return', (event) => {
+    let link = results[resultIndex];
+    location.href = link.attr('href');
+    event.stopPropagation();
+    event.preventDefault();
+  });
+  let searchInput = document.getElementById('lst-ib');
+  key('/', (event) => {
+    searchInput.focus();
+    event.stopPropagation();
+    event.preventDefault();
+  });
+};
+
+// This file is loaded only after the DOM is ready, so no need to wait for
+// DOMContentLoaded.
+addEventListeners();
