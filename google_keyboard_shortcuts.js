013a5cbd3c3823fee9f9928c842f7d734709a433
Marcos <marcoscarvalhodeassis@gmail.com>
2017-10-03 09:44:10 -0300

    Restore the focused item when using back

--- google_keyboard_shortcuts.js
+++ google_keyboard_shortcuts.js
@@ -1,4 +1,5 @@
 let options;
+let lastNavigation;
 
 const toggleResultHighlighting = (link) => {
   link.classList.toggle('highlighted-search-result')
@@ -34,6 +35,24 @@ const loadOptions = () => {
   });
 };
 
+const loadLastNavigation = () => {
+  return new Promise((resolve, reject) => {
+    chrome.storage.local.get(
+    {
+      lastQueryUrl: false,
+      lastFocusedIndex: 0
+    },
+    (items) =>{
+      lastNavigation = items;
+      if (chrome.runtime.lastError) {
+        reject();
+      } else {
+        resolve();
+      }
+    });
+  });
+}
+  
 const getNextIndex = (currentIndex, numResults, shouldWrap) => {
   if (currentIndex < numResults - 1) {
     return currentIndex + 1;
@@ -69,6 +88,12 @@ const initResultsNavigation = (results) => {
     // Highlight the first result when the page is loaded.
     updateHighlightedResult(0);
   }
+  loadLastNavigation().then(() => {
+    if (location.href === lastNavigation.lastQueryUrl) {
+      isFirstNavigation = false;
+      updateHighlightedResult(lastNavigation.lastFocusedIndex);
+    }
+  });
   key(options.nextKey, (event) => {
     let nextIndex =
         getNextIndex(resultIndex, results.length, options.wrapNavigation);
@@ -91,7 +116,7 @@ const initResultsNavigation = (results) => {
   });
   key(options.navigateKey, (event) => {
     let link = results[resultIndex];
-    link.click();
+    saveLastNavigation();
     handleEvent(event);
   });
   key(options.navigateNewTabKey, (event) => {
@@ -214,6 +239,15 @@ function getElementByXpath(path) {
       .evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null)
       .singleNodeValue;
 };
+
+const saveLastNavigation = () => {
+  chrome.storage.local.set(
+      {
+        lastQueryUrl: location.href,
+        lastFocusedIndex: resultIndex 
+      }, null);
+    link.click();
+}
 
 initPageIfNeeded();
 
