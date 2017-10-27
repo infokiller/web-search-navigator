36164944e99bd5e66b46d43ba5e98a88a3b069de
Michal Novák <michal.novak@weborama.cz>
2017-10-27 12:14:46 +0200

    update the make-firefox script with the new directory structure
    Actually the script is almost useless as the web extension files now do not mix with another files.

--- tools/make-firefox.sh
+++ tools/make-firefox.sh
@@ -25,12 +25,8 @@ echo "Copying files..."
 rm -rf "$OBJ"
 mkdir -p "$OBJ"
 
-cp *.css "$OBJ"
-cp *.html "$OBJ"
-cp *.js "$OBJ"
-cp *.json "$OBJ"
+cp src/* "$OBJ"
 cp .web-extension-id "$OBJ"
-cp icon*.png "$OBJ"
 
 # pack and sign the package
 echo "Creating package..."
