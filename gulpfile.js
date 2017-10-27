67f7a3621a822c130dd6bf0c59a9f6f144c5498d
Michal Novák <michal.novak@weborama.cz>
2017-10-27 11:55:21 +0200

    preserve the comments (license information) when uglifying the javascript files

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,6 @@ const util = require('gulp-util');
 
 gulp.task('default', () => {
   return gulp.src('node_modules/keymaster/keymaster.js')
-    .pipe(util.env.production ? uglify() : util.noop())
+    .pipe(util.env.production ? uglify({output: {comments: true}}) : util.noop())
     .pipe(gulp.dest('./src'));
 });
