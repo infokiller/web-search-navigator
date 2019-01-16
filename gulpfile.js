b113fac464a0031f7d479b2182250fb310a9549f
Ty Kroll <tkroll@gmail.com>
2019-01-16 16:20:22 +0700

    Changed to keymaster, udpdated README, bumped to 0.3.4, removed focus outline for Chrome
    Put back the outline re: comments on /issues/1583
    
    Add option to hide select result outline
    
    Recommended fixes
    
    Update gulpfile.js
    
    Changes requested on 12 Mar
    
    https://github.com/infokiller/web-search-navigator/pull/101#issuecomment-471603895
    
    update dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,14 @@
 const gulp = require('gulp');
 const uglify = require('gulp-uglify');
 const util = require('gulp-util');
+const moustrapJsFiles = [
+	'node_modules/mousetrap/mousetrap.js',
+	// Global bind requires to handle keypresses inside search box
+	'node_modules/mousetrap/plugins/global-bind/mousetrap-global-bind.js'
+]
 
 gulp.task('default', () => {
-  return gulp.src('node_modules/keymaster/keymaster.js')
-    .pipe(util.env.production ? uglify({output: {comments: true}}) : util.noop())
-    .pipe(gulp.dest('./src'));
-});
+	  return gulp.src(moustrapJsFiles)
+	    .pipe(util.env.production ? uglify({output: {comments: true}}) : util.noop())
+	    .pipe(gulp.dest('./src'));
+	});
