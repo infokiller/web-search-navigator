7301acbca1d9158bcc7ba9131eae148defa1a7f4
Yoni Weill <joweill@icloud.com>
2020-06-05 19:24:39 +0300

    Update libraries, add browser-polyfill and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es6': true,
+  },
+  'extends': [
+    'google',
+  ],
+  'globals': {
+    'Atomics': 'readonly',
+    'SharedArrayBuffer': 'readonly',
+  },
+  'parserOptions': {
+    'ecmaVersion': 11,
+  },
+  'rules': {
+  },
+};
