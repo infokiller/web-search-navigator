601da5a09f2c5fb4fe8ade0012c8aef6c39bd8c7
Yoni Weill <joweill@icloud.com>
2020-10-09 14:39:06 +0300

    refactor: single quotes

--- tools/make-chrome.sh
+++ tools/make-chrome.sh
@@ -4,12 +4,12 @@
 # This script builds the extension for the Chrome browser.
 #
 ##
-echo "Building Web Search Navigator for Chrome"
+echo 'Building Web Search Navigator for Chrome'
 
 # copy the sources into the working directory
 BIN=build/chrome
 OBJ="$BIN/obj"
-echo "Copying files..."
+echo 'Copying files...'
 
 # cleanup the previous build
 rm -rf "$OBJ"
@@ -17,7 +17,7 @@ mkdir -p "$OBJ"
 
 cp -R src/* "$OBJ"
 
-echo "Creating package..."
+echo 'Creating package...'
 zip -FSj "$BIN/package.zip" $OBJ/*
 
-echo "Build complete"
+echo 'Build complete'
