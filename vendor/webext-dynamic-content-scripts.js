0af541853299ada92541e0f6ea14715ede44c71a
Andreas <962868+Anteus@users.noreply.github.com>
2022-11-04 11:38:40 +0100

    patch executeScript for firefox
    #450 tested in Firefox

--- vendor/webext-dynamic-content-scripts.js
+++ vendor/webext-dynamic-content-scripts.js
@@ -96,7 +96,7 @@
 	            if ('code' in content) {
 	                throw new Error('chrome.scripting does not support injecting strings of `code`');
 	            }
-	            void chrome.scripting.executeScript({
+	            void chromeP$1.scripting.executeScript({
 	                target: {
 	                    tabId,
 	                    frameIds: arrayOrUndefined(frameId),
