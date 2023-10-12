60d33e65e056d9e6f544ebb53a1f211e54ad4c45
--Explosion-- <explosion.scratch@gmail.com>
2023-10-12 00:24:41 +0000

    Options page

--- src/userscript-options.js
+++ src/userscript-options.js
@@ -1,57 +1,92 @@
-const OPTIONS_HTML = `
+const OPTIONS_HTML = atob(`
 
 __OPTIONS_HTML__
 
-`
+`.replaceAll('\n', ''));
 
-const OPTIONS_CSS = `
+const OPTIONS_CSS = atob(`
 
 __OPTIONS_CSS__
 
-`
+`.replaceAll('\n', ''));
 
-const OPTIONS_JS = `
+const OPTIONS_JS = atob(`
 
 __OPTIONS_JS__
 
-`
+`.replaceAll('\n', ''));
 
-function showOptions(){
-    const CONTAINER_ID = 'webNavigatorIframe';
-    if (document.getElementById(CONTAINER_ID)){
-        document.getElementById(CONTAINER_ID).remove();
-    }
-    const iframe = document.createElement('iframe');
-    const iframe_container = document.createElement('div');
+const OPTIONS_PAGE_JS = atob(`
 
-    iframe_container.id = CONTAINER_ID;
-    iframe_container.onclick = () => {
-        iframe_container?.remove();
-    }
-    iframe.onclick = (e) => {
-        e.stopPropagation();
-    }
-    iframe.srcdoc = OPTIONS_HTML;
-    Object.assign(iframe_container.style, {
-        position: 'fixed',
-        display: 'grid',
-        cursor: 'pointer',
-        placeItems: 'center',
-        inset: 0,
-        backgroundColor: '#0003',
-        zIndex: 100000,
-    })
-    iframe_container.appendChild(iframe);
-    Object.assign(iframe.style, {
-        width: '80vw',
-        height: '80vh',
-        border: 'none',
-        borderRadius: '3px',
-        overflow: 'hidden',
-        background: '#fff',
-    })
-    document.body.appendChild(iframe_container);
-    return {el: iframe, container: iframe_container}
+__OPTIONS_PAGE_JS__
+
+`.replaceAll('\n', ''));
+
+const BROWSER_POLYFILL_JS = atob(`
+
+__BROWSER_POLYFILL_JS__
+
+`.replaceAll('\n', ''));
+
+function showOptions() {
+  const CONTAINER_ID = "webNavigatorIframe";
+  if (document.getElementById(CONTAINER_ID)) {
+    document.getElementById(CONTAINER_ID).remove();
+  }
+  const iframe = document.createElement("iframe");
+  const iframe_container = document.createElement("div");
+
+  iframe_container.id = CONTAINER_ID;
+  iframe_container.onclick = () => {
+    iframe_container?.remove();
+  };
+  iframe.onclick = (e) => {
+    e.stopPropagation();
+  };
+  const OUT_HTML = OPTIONS_HTML
+    .replaceAll(`<script src="options.js"></script>`, `<script>\n\n${OPTIONS_JS}\n\n</script>`)
+    .replaceAll(`<script src="options_page.js"></script>`, `<script>\n\n${OPTIONS_PAGE_JS}\n\n</script>`)
+    .replaceAll(`<script src="browser-polyfill.js"></script>`, `<script>\n\n${BROWSER_POLYFILL_JS}\n\n</script>`)
+    .replaceAll(`<link rel="stylesheet" href="options_page.css">`, `<style>\n\n${OPTIONS_CSS}\n\n</style>`);
+
+  console.log({OUT_HTML});
+  iframe.srcdoc = OUT_HTML;
+  Object.assign(iframe_container.style, {
+    position: "fixed",
+    display: "grid",
+    cursor: "pointer",
+    placeItems: "center",
+    inset: 0,
+    backgroundColor: "#0003",
+    zIndex: 100000,
+  });
+  iframe_container.appendChild(iframe);
+  Object.assign(iframe.style, {
+    width: "80vw",
+    height: "80vh",
+    border: "none",
+    borderRadius: "3px",
+    overflow: "hidden",
+    background: "#fff",
+  });
+  document.body.appendChild(iframe_container);
+  return { el: iframe, container: iframe_container };
 }
 
-showOptions();
+// TODO: Make the options page use postMessage to parent and localStorage to utilize settings
+
+function blobToDataURL(blob) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      resolve(reader.result);
+    };
+    reader.onerror = function (e) {
+      reject(reader.error);
+    };
+    reader.onabort = function (e) {
+      reject(new Error("Read aborted"));
+    };
+    reader.readAsDataURL(blob);
+  });
+}
--- tools/make-userscript.sh
+++ tools/make-userscript.sh
@@ -4,12 +4,16 @@ cat $(cat ../src/manifest.json | jq -r ".content_scripts[0].js | map(\"$PTH/\"+.
 
 USERSCRIPT_OPTIONS=$(cat "$PTH/userscript-options.js")
 
-OPTIONS_HTML=$(cat "$PTH/options_page.html")
-OPTIONS_CSS=$(cat "$PTH/options_page.css")
-OPTIONS_JS=$(cat "$PTH/options_page.js")
+OPTIONS_HTML=$(cat "$PTH/options_page.html" | base64)
+OPTIONS_CSS=$(cat "$PTH/options_page.css" | base64)
+OPTIONS_PAGE_JS=$(cat "$PTH/options_page.js" | base64)
+OPTIONS_JS=$(cat "$PTH/options.js" | base64)
+BROWSER_POLYFILL_JS=$(cat "$PTH/userscript-polyfill.js" "$PTH/browser-polyfill.js" | base64)
 
 FINAL=$(echo "${USERSCRIPT_OPTIONS//__OPTIONS_HTML__/$OPTIONS_HTML}")
 FINAL=$(echo "${FINAL//__OPTIONS_CSS__/$OPTIONS_CSS}")
 FINAL=$(echo "${FINAL//__OPTIONS_JS__/$OPTIONS_JS}")
+FINAL=$(echo "${FINAL//__OPTIONS_PAGE_JS__/$OPTIONS_PAGE_JS}")
+FINAL=$(echo "${FINAL//__BROWSER_POLYFILL_JS__/$BROWSER_POLYFILL_JS}")
 
-echo "$FINAL"
+echo "$FINAL" > "build/userscript/userscript-options.js"
